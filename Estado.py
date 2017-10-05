04b008d165c39bb04b3279ec8a364a80d98c5a82
pasquinid <dpasquini1@gmail.com>
2017-10-05 12:49:16 -0300

    agora usa-se apenas estados e da para saber, na lista dos estados possiveis, a proxima posicao do aspirador > . Adicionei funcao verifica objetivo para saber se acaba a busca. OBS: agora sempre que ouver movimentação do agente, a função do movimento retornará um estado novo, e este novo estado sera armazenado numa array caminho que estaria na MAIN.

--- Estado.py
+++ Estado.py
@@ -1,69 +1,122 @@
 from Ambiente import Ambiente
 class Estado:
 	estados_possiveis = []
-
+	estado_pai = []
 	def explora_no(self):  # visualizar os estados possiveis a partir do atual
 		
 		self.estados_possiveis = [] # limpando os estados possiveis para que nao tenha conteudo de um no anterior
 		
 		if(self.atual.getLocal_especifico(self.pos_x,self.pos_y) != None):
 			if(self.atual.getLocal_especifico(self.pos_x,self.pos_y).sujo == True): # proximo estado pode ser o meu quadrado limpo?
-				est_auxiliar = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
-				est_auxiliar.getLocal_especifico(self.pos_x,self.pos_y).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar = Estado(self.atual.num_quadrados,self.pos_x,self.pos_y)
+				est_auxiliar.atual = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
+				est_auxiliar.atual.getLocal_especifico(self.pos_x,self.pos_y).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar.setPai(self)
 				self.estados_possiveis.append(est_auxiliar) #adiciona ele aos estados possiveis
 
 		if(self.atual.getLocal_especifico(self.pos_x+1,self.pos_y) != None):
 			if(self.atual.getLocal_especifico(self.pos_x+1,self.pos_y).sujo == True): # proximo estado pode ser o quadrado A MINHA DIREITA limpo?
-				est_auxiliar = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
-				est_auxiliar.getLocal_especifico(self.pos_x+1,self.pos_y).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar = Estado(self.atual.num_quadrados,self.pos_x+1,self.pos_y)
+				est_auxiliar.atual = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
+				est_auxiliar.atual.getLocal_especifico(self.pos_x+1,self.pos_y).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar.setPai(self)
 				self.estados_possiveis.append(est_auxiliar) #adiciona ele aos estados possiveis
 
 		if(self.atual.getLocal_especifico(self.pos_x-1,self.pos_y) != None):
 			if(self.atual.getLocal_especifico(self.pos_x-1,self.pos_y).sujo == True): # proximo estado pode ser o quadrado A MINHA ESQUERDA limpo?
-				est_auxiliar = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
-				est_auxiliar.getLocal_especifico(self.pos_x-1,self.pos_y).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar = Estado(self.atual.num_quadrados,self.pos_x-1,self.pos_y)
+				est_auxiliar.atual = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
+				est_auxiliar.atual.getLocal_especifico(self.pos_x-1,self.pos_y).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar.setPai(self)
 				self.estados_possiveis.append(est_auxiliar) #adiciona ele aos estados possiveis
 		
 		if(self.atual.getLocal_especifico(self.pos_x,self.pos_y+1) != None):	
 			if(self.atual.getLocal_especifico(self.pos_x,self.pos_y+1).sujo == True): # proximo estado pode ser o quadrado A CIMA limpo?
-				est_auxiliar = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
-				est_auxiliar.getLocal_especifico(self.pos_x,self.pos_y+1).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar = Estado(self.atual.num_quadrados,self.pos_x,self.pos_y+1)
+				est_auxiliar.atual = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
+				est_auxiliar.atual.getLocal_especifico(self.pos_x,self.pos_y+1).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar.setPai(self)
 				self.estados_possiveis.append(est_auxiliar) #adiciona ele aos estados possiveis
 		
 		if(self.atual.getLocal_especifico(self.pos_x,self.pos_y-1) != None):
 			if(self.atual.getLocal_especifico(self.pos_x,self.pos_y-1).sujo == True): # proximo estado pode ser o quadrado A BAIXO limpo?
-				est_auxiliar = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
-				est_auxiliar.getLocal_especifico(self.pos_x,self.pos_y-1).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar = Estado(self.atual.num_quadrados,self.pos_x,self.pos_y-1)
+				est_auxiliar.atual = self.copy(self.atual) # sim , entao copia o estado (ambiente) atual e..
+				est_auxiliar.atual.getLocal_especifico(self.pos_x,self.pos_y-1).limpa() # e limpa o meu quadrado nesse estado possivel gerado (est_aux)
+				est_auxiliar.setPai(self)
 				self.estados_possiveis.append(est_auxiliar) #adiciona ele aos estados possiveis
 
 		return self.estados_possiveis
 
 	# ACOES (nunca usar acoes sem antes explorar o no) ============================
-	def andar_para_cima(self):
-		self.atual = self.copy(self.atual) 
-		self.pos_y = self.pos_y+1
+	def limpa_atual(self):
 		self.atual.getLocal_especifico(self.pos_x,self.pos_y).limpa()
 
+	def andar_para_cima(self):
+		if(self.pos_y+1 <= self.atual.ry):
+			for e in self.estados_possiveis:
+				if(e.pos_y == self.pos_y+1 and e.pos_x == self.pos_x):
+					return e
+
+			prox_estado = Estado(self.atual.num_quadrados,self.pos_x,self.pos_y+1)
+			prox_estado.atual = self.copy(self.atual)
+			prox_estado.setPai(self)
+			return prox_estado
 
 	def andar_para_baixo(self):
-		self.atual = self.copy(self.atual) 
-		self.pos_y = self.pos_y-1
-		self.atual.getLocal_especifico(self.pos_x,self.pos_y).limpa()
+		if(self.pos_y-1 >= 0):
+			for e in self.estados_possiveis:
+				if(e.pos_y == self.pos_y-1 and e.pos_x == self.pos_x):
+					return e
 
+			prox_estado = Estado(self.atual.num_quadrados,self.pos_x,self.pos_y-1)
+			prox_estado.atual = self.copy(self.atual)
+			prox_estado.setPai(self)
+			return prox_estado
 
 	def andar_para_esquerda(self):
-		self.atual = self.copy(self.atual) 
-		self.pos_x = self.pos_x-1
-		self.atual.getLocal_especifico(self.pos_x,self.pos_y).limpa()
+		if(self.pos_x-1 >= 0):
+			for e in self.estados_possiveis:
+				if(e.pos_y == self.pos_y and e.pos_x == self.pos_x-1):
+					return e
 
+			prox_estado = Estado(self.atual.num_quadrados,self.pos_x-1,self.pos_y)
+			prox_estado.atual = self.copy(self.atual)
+			prox_estado.setPai(self)
+			return prox_estado
 
 	def andar_para_direita(self):
-		self.atual = self.copy(self.atual) 
-		self.pos_x = self.pos_x+1
-		self.atual.getLocal_especifico(self.pos_x,self.pos_y).limpa()
+		if(self.pos_x+1 <= self.atual.rx):
+			for e in self.estados_possiveis:
+				if(e.pos_y == self.pos_y and e.pos_x == self.pos_x+1):
+					return e
+
+			prox_estado = Estado(self.atual.num_quadrados,self.pos_x+1,self.pos_y)
+			prox_estado.atual = self.copy(self.atual)
+			prox_estado.setPai(self)
+			return prox_estado
+
 	#==============================================================================
-	def visualizar_estado(self,amb):
-		for e in amb.locais:
+	
+	def verifica_objetivo(self):
+		count = 0
+		for q in self.atual.locais:
+			if(q.sujo == False):
+				count = count + 1
+		
+		if(count == self.atual.num_quadrados):
+			return True 
+		else:
+			return False
+
+	def setPai(self,pai):
+		self.estado_pai = pai
+
+	def getPai(self):
+		return self.estado_pai
+
+	def visualizar_estado(self,est):
+		for e in est.atual.locais:
 			print "[ x:"+str(e.x)+" y: "+str(e.y)+" sujo: "+str(e.sujo)+" ]"	
 
 	def visualizar_estado_atual(self):
@@ -76,7 +129,7 @@ class Estado:
 				print "[ x:"+str(e.x)+" y: "+str(e.y)+" sujo: "+str(e.sujo)+" ]"
 	
 	def copy(self,Amb):
-		novo_amb = Ambiente(int(Amb.getQuadrados()),Amb)
+		novo_amb = Ambiente(int(Amb.getQuadrados()))
 		count = 0
 
 		for l in Amb.getLocais():
@@ -88,6 +141,7 @@ class Estado:
 		return novo_amb
 
 	def __init__(self,num_quadrados,pos_do_asp_X,pos_do_asp_Y):
-		self.atual = Ambiente(num_quadrados,'raiz')
+		self.atual = Ambiente(num_quadrados)
 		self.pos_x = pos_do_asp_X
 		self.pos_y = pos_do_asp_Y
+		self.estado_pai = 'raiz'
