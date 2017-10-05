7191e5bbad4e2b99c46673e5e337ab696dd77957
Diego de Pontes Pasquini <dpasquini1@gmail.com>
2017-10-05 12:58:13 -0300

    Update Ambiente.py

--- Ambiente.py
+++ Ambiente.py
@@ -7,7 +7,8 @@ class Ambiente:
 	locais = []
 
 	num_quadrados = 0
-	ambiente_pai = []
+	
+
 
 	def getLocal_especifico(self,x,y):
 		for i in self.locais:
@@ -20,8 +21,8 @@ class Ambiente:
 	def getQuadrados(self):
 		return int(self.num_quadrados)
 	
-	def __init__(self,num_quadrados,pai):
-		self.ambiente_pai = pai
+	def __init__(self,num_quadrados):
+		
 		if(num_quadrados%2 != 0):
 			print("Valor impar!")
 			return
@@ -29,14 +30,14 @@ class Ambiente:
 		tam_x = int(math.sqrt(num_quadrados))
 		tam_y = num_quadrados - tam_x
 
-		rx = range(tam_x)
-		ry = range(tam_y)
+		self.rx = range(tam_x)
+		self.ry = range(tam_y)
 
 		self.locais = [Quadrado(True,1,1) for i in range(num_quadrados)]
 		
 		count = 0 
-		for x in rx:
-			for y in ry:				
+		for x in self.rx:
+			for y in self.ry:				
 				self.locais[count].x = x
 				self.locais[count].y = y
 				self.locais[count].sujo = random.choice([True, False])
